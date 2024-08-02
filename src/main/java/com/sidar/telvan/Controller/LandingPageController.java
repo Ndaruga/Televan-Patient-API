9fdde2cd6fbdbc8dc11433780e7ebd471c464fe4
ndaruga <ndaruga.francis@gmail.com>
2024-08-02 20:29:04 +0300

    successfull web app

--- src/main/java/com/sidar/telvan/Controller/LandingPageController.java
+++ src/main/java/com/sidar/telvan/Controller/LandingPageController.java
@@ -10,4 +10,19 @@ public class LandingPageController {
     public String landingPage() {
         return "index";
     }
+
+    @GetMapping("/create-patient")
+    public String createPatient() {
+        return "create-patient";
+    }
+
+    @GetMapping("/list-patients")
+    public String listPatients() {
+        return "list-patients";
+    }
+
+    @GetMapping("/update-patient")
+    public String updatePatient() {
+        return "update-patient";
+    }
 }
